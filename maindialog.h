c15b7426deff716ea589fa7f4edf2c654aa84988
Jakob Schöttl <jschoett@gmail.com>
2016-11-07 12:53:30 +0100

    Move definitions into header file

--- maindialog.h
+++ maindialog.h
@@ -1,10 +1,14 @@
 #ifndef MAINDIALOG_H
 #define MAINDIALOG_H
 
-#include <QList>
-#include <QDialog>
+#include "ui_maindialog.h"
 #include "buttonconfig.h"
 #include "mybutton.h"
+#include <QDebug>
+#include <QWidget>
+#include <QList>
+#include <QDialog>
+
 
 namespace Ui {
 class MainDialog;
@@ -14,14 +18,37 @@ class MainDialog : public QDialog
 {
     Q_OBJECT
 
-public:
-    explicit MainDialog(QWidget *parent = 0);
-    explicit MainDialog(const QList<ButtonConfig>&);
-    ~MainDialog();
-
 private:
     Ui::MainDialog *ui;
     QList<MyButton*> buttonList;
+
+public:
+
+    explicit MainDialog(QWidget *parent = NULL) :
+        QDialog(parent),
+        ui(new Ui::MainDialog)
+    {
+        ui->setupUi(this);
+    }
+
+    explicit MainDialog(const QList<ButtonConfig> &list) :
+        MainDialog((QWidget*) NULL)
+    {
+        int insertIndex = 0;
+        foreach (const ButtonConfig& bc, list)
+        {
+            qDebug() << bc.caption;
+            MyButton* button = new MyButton(bc);
+            QObject::connect(button, SIGNAL(clicked()), this, SLOT(buttonClicked()));
+            buttonList.append(button);
+            ui->buttonlist->insertWidget(insertIndex++, button);
+        }
+    }
+
+    ~MainDialog()
+    {
+        delete ui;
+    }
 private slots:
     void buttonClicked()
     {
